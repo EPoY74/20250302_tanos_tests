c0c4f32ca2bb66c66a0a96c8549c1faad305d548
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-02 22:34:59 +0500

    Добавил тестирование https://tanos-cp.dt-teh.ru/api/passes/v1/pass-type-by-time-of-the-day/list

--- tests/test_tanos_dt_teh_ru_sec.py
+++ tests/test_tanos_dt_teh_ru_sec.py
@@ -104,5 +104,26 @@ class TestUnautorezedAccessPassesV1(unittest.TestCase):
         self.assertEqual(responce_data["message"], 
                          "Unauthorized", 
                           f"Ожидается  \"Unauthorized\" получено {responce_data['message']}")
+
+
+    def test_unauth_access_time_day_list(self):
+        """
+            Тестирование эндпоинта https://tanos-cp.dt-teh.ru/api/passes/v1/pass-type-by-time-of-the-day/list
+        """
+        url: str ="https://tanos-cp.dt-teh.ru/api/passes/v1/pass-type-by-time-of-the-day/list"
+        responce: Response = requests.get(url)
+        self.assertEqual(responce.status_code, 
+                         401, 
+                         f"Ошибка авторизации или неверные учетные данные: {responce.status_code}")
+        responce_data = responce.json()
+        self.assertIn("message",
+                      responce_data,
+                      "Ключ message не найден в ответе от сервера")
+        
+        self.assertEqual(responce_data["message"], 
+                         "Unauthorized", 
+                          f"Ожидается  \"Unauthorized\" получено {responce_data['message']}")
+
+
 if __name__ == "__main__":
     unittest.main()
