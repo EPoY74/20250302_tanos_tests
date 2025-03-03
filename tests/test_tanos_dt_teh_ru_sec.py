806ccc0a69bd6d4f822f0e942d6e779bef7179d7
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-03 07:25:53 +0500

    Добавил тестирование https://tanos-cp.dt-teh.ru/api/passes/v1/create-or-update/transport_owner_by_vrc

--- tests/test_tanos_dt_teh_ru_sec.py
+++ tests/test_tanos_dt_teh_ru_sec.py
@@ -279,5 +279,24 @@ class TestUnautorezedAccessPassesV1(unittest.TestCase):
                           f"Ожидается  \"Unauthorized\" получено {responce_data['message']}")
 
 
+    def test_unauth_access_post_creat_upd_trans_own(self):
+        """
+            Тестирование эндпоинта https://tanos-cp.dt-teh.ru/api/passes/v1/create-or-update/transport_owner_by_vrc
+        """
+        url: str ="https://tanos-cp.dt-teh.ru/api/passes/v1/create-or-update/transport_owner_by_vrc"
+        responce: Response = requests.post(url)
+        self.assertEqual(responce.status_code, 
+                         401, 
+                         f"Ошибка авторизации или неверные учетные данные: {responce.status_code}")
+        responce_data = responce.json()
+        self.assertIn("message",
+                      responce_data,
+                      "Ключ message не найден в ответе от сервера")
+        
+        self.assertEqual(responce_data["message"], 
+                         "Unauthorized", 
+                          f"Ожидается  \"Unauthorized\" получено {responce_data['message']}")
+
+
 if __name__ == "__main__":
     unittest.main()
