4143dcb3b13618bb5746401ff7677a21ea2eeeb5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-06 16:14:35 +0500

    Начало реализации проверки значания в ответе json

--- tests/test_tanos_auth.py
+++ tests/test_tanos_auth.py
@@ -3,6 +3,7 @@
 """
 
 import unittest
+import json
 
 import requests
 from requests import Response
@@ -11,6 +12,11 @@ import settings_local
 
 user_name: str = settings_local.user1
 password: str = settings_local.password1
+
+
+def get_value_from_json(json_date, key):
+    data = json.loads(json_date)
+    return data.get(key)
 
 
 class TestAytorizedAccess(unittest.TestCase):
@@ -36,18 +42,18 @@ class TestAytorizedAccess(unittest.TestCase):
         try:
             responce: Response = requests.post(self.auth_url,
                                                json = self.auth_payload,
-                                               timeout = 3)
+                                               timeout = 5)
             responce.raise_for_status()
             token: str|None = responce.json().get("access")
             self.assertIsNotNone(token,
                                   "Токен не найден в ответе.")
+            print(token)
             return token
 
         except requests.exceptions.RequestException as err :
             self.fail (f"Ошибка при получении токена {err} ")
-    
-    # def test_print(self):
-    #     print(str(self.token))
+
+
     def test_auth_acces_passes_list(self):
         """
         Проверяю возвращаемые ключи в эндпоинте https://tanos-cp.dt-teh.ru/api/passes/v1/list 
@@ -56,19 +62,29 @@ class TestAytorizedAccess(unittest.TestCase):
         try:
             response: Response = requests.get(self.api_url,
                                             headers = headers,
-                                            timeout = 3)
+                                            timeout = 5)
             self.assertEqual(response.status_code,
                             200,
-                            f"Ошибка при запросе, код: {response.status_code}")
-            # self.assertIn("car_id",
-            #             response.json()['items'],
-            #             f"Ключ  car_id не найден в ответе.")
+                            f"Ошибка при запросе, код: {response.status_code} \n json Ответа: {response.json()} ")
             car_id_present: int = 0
             car_id_present = sum( 'car_id' in item for item in response.json()['items'])
             self.assertTrue(car_id_present, "Ключ 'car_id' не найден в ответе")
             print(f"Ключ 'car_id' найден в {car_id_present} элементах из {len(response.json()['items'])} ")
+            
+            for item in response.json()['items']:
+                print(item)
+                # print(typeof(item))
+                car_id_data = get_value_from_json(item, "car_id")
+                self.assertIsInstance(car_id_data, int, f'Значение {car_id_data} должно быть integer')
+
+            reg_num_present: int = 0
+            reg_num_present = sum( 'reg_num' in item for item in response.json()['items'])
+            self.assertTrue(reg_num_present, "Ключ 'reg_num' не найден в ответе")
+            print(f"Ключ 'reg_num' найден в {reg_num_present} элементах из {len(response.json()['items'])} ")
+
+
         except requests.exceptions.RequestException as err:
-            self.fail(f"Ошибка при обращении к защищенному endpoint {err}")
+            self.fail(f"\nОшибка при обращении к endpoint: {err}")
 
 
 if __name__ == "__main__":
