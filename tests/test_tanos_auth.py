8075f71c1c3fb16b7f8baee33397b80a191c8476
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-11 16:39:59 +0500

    Поправил форматирование

--- tests/test_tanos_auth.py
+++ tests/test_tanos_auth.py
@@ -2,7 +2,9 @@
 Тестирование endpoints с авторизацией
 """
 
+import errno
 import json
+import logging
 import unittest
 from typing import Optional
 
@@ -13,6 +15,7 @@ import settings_local
 
 user_name: str = settings_local.user1
 password: str = settings_local.password1
+logging.basicConfig(level=logging.DEBUG)
 
 
 def get_value_from_json(json_date, key):
@@ -35,7 +38,7 @@ class TestAytorizedAccess(unittest.TestCase):
         cls.token = cls.get_bearer_token()
 
     @classmethod
-    def get_bearer_token(cls) -> Optional[str]:
+    def get_bearer_token(cls) -> str | None:
         """
         Получение токена авторизации
         """
@@ -45,14 +48,11 @@ class TestAytorizedAccess(unittest.TestCase):
             )
             responce.raise_for_status()
             token = responce.json().get("access")
-            # cls.assertIsNotNone(cls.token,
-            #                       "Токен не найден в ответе.")
-            print()
-            print(token)
+            logging.debug(token)
             return token
 
         except requests.exceptions.RequestException as err:
-            print(f"Ошибка при получении токена {err} ")
+            logging.error(f"Ошибка при получении токена {err} ")
             return None
 
     def SetUp(self):
@@ -60,37 +60,56 @@ class TestAytorizedAccess(unittest.TestCase):
         self.token: str | None = self.__class__.token
         self.api_url: str = self.__class__.api_url
 
-    # def test_auth_acces_pas_list(self):
-    #     """
-    #     Проверяю возвращаемые ключи в эндпоинте https://tanos-cp.dt-teh.ru/api/passes/v1/list
-    #     """
-    #     headers: dict = {'Authorization' : f'Bearer {self.token}'}
-    #     response: Response|None = None
-    #     try:
-    #         response = requests.get(self.api_url,
-    #                                         headers = headers,
-    #                                         timeout = 5)
-    #         self.assertEqual(response.status_code,
-    #                         200,
-    #                         f"Ошибка при запросе, код: {response.status_code} \n json Ответа: {response.json()} ")
-    #         car_id_present: int = 0
-    #         car_id_present = sum( 'car_id' in item for item in response.json()['items'])
-    #         self.assertTrue(car_id_present, "Ключ 'car_id' не найден в ответе")
-    #         print(f"Ключ 'car_id' найден в {car_id_present} элементах из {len(response.json()['items'])} ")
+    def test_auth_acces_pas_list(self):
+        """
+        Проверяю возвращаемые ключи 
+          в эндпоинте https://tanos-cp.dt-teh.ru/api/passes/v1/list
+        """
+        headers: dict = {'Authorization' : f'Bearer {self.token}'}
+        response: Response|None = None
+        try:
+            response = requests.get(self.api_url,
+                                            headers = headers,
+                                            timeout = 5)
+            err_msg = ( f"Ошибка при запросе, код: {response.status_code} \n"
+                        f"json Ответа: {response.json()} "
+                        )
+            self.assertEqual(response.status_code,
+                            200, err_msg)
+        
+            car_id_present: int = 0
+            car_id_present = sum( 'car_id' 
+                                 in item for item 
+                                 in response.json()['items']
+                                 )
+            self.assertTrue(car_id_present, "Ключ 'car_id' не найден в ответе")
+            err_msg = (f"Ключ 'car_id' найден в {car_id_present} "
+                       f"элементах из {len(response.json()['items'])} ")
+            logging.error(err_msg)
 
-    #         for item in response.json()['items']:
-    #             print(item)
-    #             # print(typeof(item))
-    #             car_id_data = item
-    #             self.assertIsInstance(car_id_data, int, f'Значение {car_id_data} должно быть integer')
+            for item in response.json()['items']:
+                logging.debug(item)
+                car_id_data = item
+                err_msg = ( 
+                    f'Значение {car_id_data} должно быть integer')
+                self.assertIsInstance(car_id_data, 
+                                      int, err_msg)
 
-    #         reg_num_present: int = 0
-    #         reg_num_present = sum( 'reg_num' in item for item in response.json()['items'])
-    #         self.assertTrue(reg_num_present, "Ключ 'reg_num' не найден в ответе")
-    #         print(f"Ключ 'reg_num' найден в {reg_num_present} элементах из {len(response.json()['items'])} ")
+            reg_num_present: int = 0
+            reg_num_present = sum('reg_num' 
+                                  in item for item 
+                                  in response.json()['items']
+                                  )
+            err_msg = "Ключ 'reg_num' не найден в ответе"
+            self.assertTrue(reg_num_present, errno)
+            info_msg = (f"Ключ 'reg_num' найден в {reg_num_present} "
+                        f"элементах из {len(response.json()['items'])} "
 
-    #     except requests.exceptions.RequestException as err:
-    #         self.fail(f"\nОшибка при обращении к endpoint: {err}")
+            )
+            logging.debug(info_msg)
+
+        except requests.exceptions.RequestException as err:
+            self.fail(f"\nОшибка при обращении к endpoint: {err}")
 
     def test_auth_pass_app_list(self):
         """
@@ -102,12 +121,15 @@ class TestAytorizedAccess(unittest.TestCase):
         response: Response | None = None
         try:
             response = requests.get(self.api_url, headers=headers, timeout=5)
+            err_msg = (
+                f"Ошибка при запросе, ожидается код ответа 200 \n"
+                f"Код: {response.status_code} \n "
+                f"json Ответа: {response.json()}"
+            )
             self.assertEqual(
                 response.status_code,
                 200,
-                f"Ошибка при запросе, ожидается код ответа 200 \nКод: {response.status_code} \n json Ответа: {response.json()} ",
-            )
-
+                err_msg)
         except requests.exceptions.RequestException as err:
             self.fail(f"\nОшибка при обращении к endpoint: {err}")
 
