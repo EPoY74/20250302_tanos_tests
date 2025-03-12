86449c4ab0c91edf2809cda72b28cce457af7a4c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-03-12 09:28:10 +0500

    Рефакторинг и чистка кода

--- tests/test_tanos_auth_missing_param.py
+++ tests/test_tanos_auth_missing_param.py
@@ -1,5 +1,7 @@
 """
-Тестирование endpoints с авторизацией
+Тестирование endpoints с авторизацией, 
+но без ввода обязательных параметров. 
+Смотрим, что возврящается, если их не передать
 """
 
 import logging
@@ -17,18 +19,23 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class TestAytorizedAccess(unittest.TestCase):
+    """
+    Класс для тесторования API с неуказанными обязательными параметрами
+    """
     token: str | None = None
 
+
     @classmethod
-    def setUpClass(cls):
-        cls.api_url: str = ""
+    def setUpClass(cls) -> None:
         """
         Тестирую  url https://tanos-cp.dt-teh.ru/api/passes/v1/list
-        Базовые настройки для для всего класса 
+        Базовые настройки для всего класса 
         """
+        cls.api_url: str = ""
         cls.auth_url: str = "https://tanos-cp.dt-teh.ru/api/token/pair"
         cls.auth_payload: dict = {"phone": user_name, "password": password}
         cls.token = cls.get_bearer_token()
+
 
     @classmethod
     def get_bearer_token(cls) -> str | None:
@@ -36,19 +43,19 @@ class TestAytorizedAccess(unittest.TestCase):
         Получение токена авторизации один раз для всего класса
         """
         try:
-            responce: Response = requests.post(
+            response: Response = requests.post(
                 cls.auth_url, json=cls.auth_payload, timeout=5
             )
-            responce.raise_for_status()
-            token = responce.json().get("access")
+            response.raise_for_status()
+            token = response.json().get("access")
             logging.debug(token)
             return token
-
         except requests.exceptions.RequestException as err:
             logging.error(f"Ошибка при получении токена {err} ")
             return None
 
-    def SetUp(self):
+
+    def SetUp(self) -> None:
         """
         Вызавается перед каждым методом
         """
@@ -56,20 +63,20 @@ class TestAytorizedAccess(unittest.TestCase):
         self.token: str | None = self.__class__.token
         self.api_url: str = self.__class__.api_url
 
-    def test_auth_pass_list_missing(self):
+
+    def test_auth_pass_list_missing(self) -> None:
         """
         Проверяю возвращаемые ключи в эндпоинте
         https://tanos-cp.dt-teh.ru/api/passes/v1/list
         В эндпоинте присутсвуют обязательные параметры.
         Проверяю, что бы не выдавалась инфоормация, при
-        отсутствии их. Привет для тестирования111
-
+        отсутствии их.
         """
+        response: Response | None = None
         headers: dict = {"Authorization": f"Bearer {self.token}"}
         self.api_url = (
             "https://tanos-cp.dt-teh.ru/api/passes/v1/application/list"
         )
-        response: Response | None = None
         try:
             response = requests.get(self.api_url, headers=headers, timeout=5)
             self.assertEqual(
@@ -80,11 +87,11 @@ class TestAytorizedAccess(unittest.TestCase):
                     f"json Ответа: {response.json()}  "
                 ),
             )
-
         except requests.exceptions.RequestException as err:
             self.fail(f"\nОшибка при обращении к endpoint: {err}")
 
-    def test_auth_pass_app_list_missing(self):
+
+    def test_auth_pass_app_list_missing(self) -> None:
         """
         Проверяю возвращаемые ключи в эндпоинте
         https://tanos-cp.dt-teh.ru/api/passes/v1/application/list
@@ -108,7 +115,6 @@ class TestAytorizedAccess(unittest.TestCase):
                     f"json Ответа: {response.json()} "
                 ),
             )
-
         except requests.exceptions.RequestException as err:
             self.fail(f"\nОшибка при обращении к endpoint: {err}")
 
